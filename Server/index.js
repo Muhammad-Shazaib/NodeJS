ba9f50d92921c0198fb32c05aba756d7e78f3560
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-07-28 22:59:55 +0500

    create a  http basic  server

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,25 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 
 const myServer = http.createServer((req, res) => {
     if(req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
        
-        switch (req.url) {
-            case '/': res.end('Home Page');
+        switch (myUrl.pathname) {
+            case '/':
+                 res.end('Home Page');
                 break;
-            case '/about': res.end('About Page');
+            case '/about': 
+               const username =myUrl.query.myname;
+            res.end(`Hi , ${username}`);         
+          //console.log(userName);
+                break;
+            case '/contact': res.end('Contact Page');
                 break;
             default: res.end('404 Not Found');
         }
