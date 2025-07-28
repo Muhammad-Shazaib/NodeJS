9c740a4907b3466322ab04f37eabfd37a06e5f43
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-07-28 23:34:13 +0500

    updated

--- Server/index.js
+++ Server/index.js
@@ -4,23 +4,28 @@ const url = require('url');
 
 
 const myServer = http.createServer((req, res) => {
-    if(req.url === '/favicon.ico') return res.end();
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    if (req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
-       
+
         switch (myUrl.pathname) {
             case '/':
-                 res.end('Home Page');
+                if (req.method) res.end('Home Page');
                 break;
-            case '/about': 
-               const username =myUrl.query.myname;
-            res.end(`Hi , ${username}`);         
-          //console.log(userName);
+            case '/about':
+                const username = myUrl.query.myname;
+                res.end(`Hi , ${username}`);
+                //console.log(userName);
                 break;
             case '/contact': res.end('Contact Page');
-                break;
+            case 'Signup':
+                if (req.method === 'GET') res.end('This is a Signup Form');
+                else if (req.method === 'POST') {
+                    // DB Query
+                    res.end('Success');
+                }
             default: res.end('404 Not Found');
         }
     })
