5ba9b5aa8ef41ef43565baa2f809f570344ff85a
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-07-29 19:20:46 +0500

    learn expressJS

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const express = require('express');
 
-
-const myServer = http.createServer((req, res) => {
+function myHandler(req, res) {
     if (req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
     const myUrl = url.parse(req.url, true);
@@ -29,7 +29,18 @@ const myServer = http.createServer((req, res) => {
             default: res.end('404 Not Found');
         }
     })
-});
+}
+
+const app = express();
+app.get('/', (req, res) => {
+   return res.end('Hello From Home Page');
+})
+
+app.get('/about', (req, res) => {
+   return res.end('Hello From About Page');
+})
+
+const myServer = http.createServer( myHandler);
 myServer.listen(8000, () => {
-    console.log("Server is listening on port 8000");
+    console.log("Server Started");
 });
