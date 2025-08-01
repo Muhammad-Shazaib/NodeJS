62849d43abf42d0e911852b9af91743ec8de5602
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-08-01 22:40:21 +0500

    middleWare update

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
+
+
 const app = express();
 const PORT = 8000;
 
 // Middleware --> Plugin
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+   fs.appendFile('log.text', `${Date.now()}:${req.ip}: ${req.method}: ${req.path}\n`, (err, data) => {
+      next()
+   })
+   // console.log('Hello From Middleware 1');
+   // req.myUserName = 'Shazaib'
+   //return res.json({msg: 'Hello From Middleware 1'})
+
+})
+
+// app.use((req, res, next) => {
+//    console.log('Hello From Middleware 2', req.myUserName);
+//    //return res.json({msg: 'Hello From Middleware 1'})
+//    //return res.end('Hey')
+//    // db query
+//    // credit card info
+//    //req.creditCardNumber = '1239'
+//    next()
+// })
 
 app.get('/users', (req, res) => {
-   const html =`
+   const html = `
    <ul>
       ${users.map(user => `<li>${user.first_name}</li>`).join('')}
    </ul>
@@ -18,7 +40,8 @@ app.get('/users', (req, res) => {
 
 // REST API
 app.get('/api/users', (req, res) => {
-   return  res.json(users);
+   // console.log('I am in get route', req.myUserName)
+   return res.json(users);
 });
 
 // app.get('/api/users/:id', (req, res) => {
@@ -41,7 +64,7 @@ app.get('/api/users', (req, res) => {
 
 app.route('/api/users/:id')
    .get((req, res) => {
-      const id = Number (req.params.id);
+      const id = Number(req.params.id);
       const user = users.find(user => user.id === id);
       return res.json(user);
    })
@@ -51,21 +74,21 @@ app.route('/api/users/:id')
    // })
    .patch((req, res) => {
       //  TODO: Edit the users with id
-      return res.json({status: 'Pending...'});
+      return res.json({ status: 'Pending...' });
    })
    .delete((req, res) => {
       //  TODO: Delete the users with id
-      return res.json({status: 'Pending...'});
+      return res.json({ status: 'Pending...' });
    });
 
 
 app.post('/api/users', (req, res) => {
-//  TODO: Create new user
-const body = req.body;
-users.push({...body, id: users.length + 1});
-fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=> {
-      return res.json({status: 'success...', id: users.length })
-})
+   //  TODO: Create new user
+   const body = req.body;
+   users.push({ ...body, id: users.length + 1 });
+   fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+      return res.json({ status: 'success...', id: users.length })
+   })
 
 });
 
